85aafe64943e6e792473ac5a147020e9752479ba
Ben C <bwc9876@gmail.com>
2023-10-21 19:11:25 -0400

    Limit the max amount of characters in username and box name

--- main/models.py
+++ main/models.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 
 class User(AbstractUser):
-    pass
+    username = models.CharField(max_length=25, unique=True)
 
 class Box(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=25)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
     private_key = models.CharField(max_length=1000)
