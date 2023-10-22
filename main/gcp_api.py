67d456d45fd6516d661be4fa6dd9e05120945ec3
Ben C <bwc9876@gmail.com>
2023-10-22 09:13:25 -0400

    Only load GCP config in DEBUG

--- main/gcp_api.py
+++ main/gcp_api.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+
 from google.oauth2 import service_account
 from google.cloud import compute_v1
 
@@ -5,7 +7,8 @@ project="annular-axe-402720"
 region="northamerica-northeast2"
 zone=f"{region}-a"
 
-credentials = service_account.Credentials.from_service_account_file('key.json')
+if settings.DEBUG:
+    credentials = service_account.Credentials.from_service_account_file('key.json')
 # If running locally. If we're in a VM, we set this to null
 
 client = compute_v1.InstancesClient(credentials=credentials)
